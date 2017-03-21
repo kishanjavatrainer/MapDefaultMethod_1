02c1f0b5d8d637aff822aaa263b1f3477042947b
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-21 21:55:31 +0530

    MapDefaultMethod_1 source code

---
+++ MapDefaultMethod_1/src/com/infotech/client/ClientTest.java
@@ -0,0 +1,22 @@
+package com.infotech.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		
+		Map<Integer,String> empMap = new HashMap<>();
+		empMap.put(190292, "Martin");
+		empMap.put(190928, "Farnk");
+		empMap.put(902292, "Andrew");
+		
+		String result = empMap.getOrDefault(10001, "Rocky");
+		System.out.println(result);
+		
+		System.out.println("----------------");
+		
+		empMap.forEach((k,v)->System.out.println(k+"\t"+v));
+	}
+}
